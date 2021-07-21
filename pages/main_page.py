0800da7a872feacda3aa6e2829ecba20bf458ff0
Viacheslav <fedorov_slava@mail.ru>
2021-07-21 12:04:50 +0300

    Lesson 4.2, step 9

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,6 +2,7 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
 
+
 class MainPage(BasePage):
 
     def go_to_login_page(self):
@@ -10,3 +11,4 @@ class MainPage(BasePage):
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
